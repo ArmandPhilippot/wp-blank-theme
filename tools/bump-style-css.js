67d287d7715096cb4662262262608ec72ea4bb4e
Armand Philippot <git@armandphilippot.com>
2022-02-09 14:55:04 +0100

    build: add scripts to bump version in style/functions on new release

---
+++ tools/bump-style-css.js
@@ -0,0 +1,19 @@
+/**
+ * bump-style-css.js
+ *
+ * Copy package.json version into style.css file.
+ */
+const fs = require( 'fs' );
+
+// Load package.json data.
+const packageJson = JSON.parse( fs.readFileSync( './package.json' ) );
+const packageVersion = packageJson.version;
+
+module.exports = {
+	files: 'style.css',
+	from: new RegExp(
+		'Version: "(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?',
+		'g'
+	),
+	to: 'Version: "' + packageVersion,
+};
